83f2729c208a262c0fe458cadc9c26ba4f6997a6
Gabriel <gabrielj_89@hotmail.com>
2021-01-05 19:18:06 -0300

    JavaFX-FXML class 01

---
+++ src/main/java/org/example/PaneReverterController.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+
+public class PaneReverterController {
+    public void revertInput(ActionEvent actionEvent) {
+        System.out.println("Ralalala");
+    }
+}
